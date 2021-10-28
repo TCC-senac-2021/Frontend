e14e55f592beb74db76c6bd89bab6676aabe7e24
Desiree <desireemenezes@moovin.com.br>
2021-10-27 23:06:23 -0300

    fix component logic

--- src/components/Content/Content.js
+++ src/components/Content/Content.js
@@ -1,8 +1,11 @@
 import React, { useState } from 'react';
 import './Content.css';
+import TypeIcons from '../Utils/TypeIcons';
 
-export default function Content() {
-	const questions = [
+
+export default function Content(props) {
+	
+	const questions = [	
 		{
 			questionText: 'Qual a faculdade te da mais desconto na próxima rematrícula?',
 			answerOptions: [
@@ -42,9 +45,22 @@ export default function Content() {
 	];
 
 	const [currentQuestion, setCurrentQuestion] = useState(0);
-	const [showScore, setShowScore] = useState(false);
 	const [score, setScore] = useState(0);
+	const [showScore, setShowScore] = useState(false);
+	const [showElement, setShowElement] = useState(false);
+	const [showAvatar, setShowAvatar] = useState(true)
+	const [type] = useState();
 
+	const man = 'Homen';
+	const woman = 'Mulher';
+	const other = 'Outros';
+
+	const handleAvatar = (index) => {
+		setShowElement(true);
+		setShowAvatar(false);
+		return index;
+	}
+  
 	const handleAnswerOptionClick = (isCorrect) => {
 		if (isCorrect) {
 			setScore(score + 1);
@@ -57,27 +73,57 @@ export default function Content() {
 			setShowScore(true);
 		}
 	};
+
+	/* const setTypeIcon = handleAvatar(index);
+	console.log(setTypeIcon)
+	 */
 	return (
-		<div className='app'>
-			{showScore ? (
-				<div className='score-section'>
-					You scored {score} out of {questions.length}
-				</div>
-			) : (
-				<>
-					<div className='question-section'>
-						<div className='question-count'>
-							<span>Question {currentQuestion + 1}</span>/{questions.length}
+		<div className="content">
+
+         {showAvatar ? (
+			<div className='avatar'>
+				{
+					TypeIcons.map((icon, index) => ( //pego o incide do vetor e substituo a imagem
+					index > 0 && 
+					<button type="button" key={index} onClick={() => handleAvatar(index)}>
+					<span className="sex"> {index === 1 ? man : index === 2 ? woman : index === 3 ? other : ''} </span>
+					<img src={icon} alt="Avatar" className={type && type !== index && 'inative'}/> 
+					</button>
+					))
+					
+				}
+			</div>
+			) : ( null )}
+			{ showElement ? (
+			<div className='app'>
+
+					{ /*  TypeIcons.map((icon, index) => (
+						index > 0 && 
+						<img src={icon} alt="Avatar" className={type && type !== index && 'inative'}/> 
+                        )) 
+					*/ }  
+
+				{showScore ? (
+					<div className='score-section'>
+						Você marcou {score} de{questions.length}
+					</div>
+				) : (
+					<>
+						<div className='question-section'>
+							<div className='question-count'>
+								<span>Question {currentQuestion + 1}</span>/{questions.length}
+							</div>
+							<div className='question-text'>{questions[currentQuestion].questionText}</div>
 						</div>
-						<div className='question-text'>{questions[currentQuestion].questionText}</div>
-					</div>
-					<div className='answer-section'>
-						{questions[currentQuestion].answerOptions.map((answerOption) => (
-							<button onClick={() => handleAnswerOptionClick(answerOption.isCorrect)}>{answerOption.answerText}</button>
-						))}
-					</div>
-				</>
-			)}
+						<div className='answer-section'>
+							{questions[currentQuestion].answerOptions.map((answerOption) => (
+								<button onClick={() => handleAnswerOptionClick(answerOption.isCorrect)}>{answerOption.answerText}</button>
+							))}
+						</div>
+					</>
+				)}
+			</div>
+			) : ( null )}
 		</div>
 	);
 }
